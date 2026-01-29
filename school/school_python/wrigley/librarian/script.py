8c71ed8732b0be5ec0fa28e853c71d8ea2ca9528
Sen Makalandawa <sen2007.maka@gmail.com>
2026-01-29 15:20:41 +0000

    continue: progress made on librarian code

--- school/school_python/wrigley/librarian/script.py
+++ school/school_python/wrigley/librarian/script.py
@@ -15,3 +15,33 @@ Main Loop: Create a menu system (using a while loop) that asks the user to:
 
 Text file: Create the text file for the project. See above for the contents. 
 '''
+
+class Book:
+    def __init__(self, author, title, isbn, isAvailable):
+        self.__author = author
+        self.__title = title
+        self.__isbn = isbn
+        self.__isAvailable = isAvailable
+    
+    def to_string(self):
+        if self.__isAvailable == True:
+            print(f"Book: {self.__title}\nAuthor: {self.__author}\nISBN: {self.__isbn}\nAvailabile: Yes")
+        else:
+            print(f"Book: {self.__title}\nAuthor: {self.__author}\nISBN: {self.__isbn}\nAvailabile: No")
+
+def menu():
+    print(f"What option would you like to do?:\n[1] List all books\n[2] Search for and checkout a book\n[3] Exit program")
+    menu_select = int(input("Choose an option 1-3: "))
+
+    exit = None
+    if menu_select == 1 or menu_select == 2:
+        exit == False
+    elif menu_select == 3:
+        exit == True
+    return exit
+
+display_menu = menu()
+if display_menu == True:
+    print("Yes")
+else:
+    print("No")
